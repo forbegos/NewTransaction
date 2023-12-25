91f4b21166fcb0b0664d5ce5f3515fc02ffaff8f
fernandodeorbegoso <fernando@fdoenterprises.com>
2023-12-25 13:28:22 -0500

    Initial Commit

---
+++ NewTransaction/NewTransactionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTransactionApp.swift
+//  NewTransaction
+//
+//  Created by Fernando de Orbegoso on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NewTransactionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
