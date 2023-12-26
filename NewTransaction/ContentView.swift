fca25d6c32d0fa859f666ddcabbd80adfd5c9eef
fernandodeorbegoso <fernando@fdoenterprises.com>
2023-12-26 08:44:52 -0500

    New transaction working - test version

--- NewTransaction/ContentView.swift
+++ NewTransaction/ContentView.swift
@@ -7,18 +7,62 @@
 
 import SwiftUI
 
+//struct Transaction {
+//    let description: String
+//    let amount: Double
+//    let type: String
+//}
+
 struct ContentView: View {
+    
+    struct Transaction {
+        let description: String
+        let amount: Double
+        let type: String
+    }
+    
+    
+    @State public var newDescription = "Description"
+    @State public var newAmount = 0.0
+    @State public var types = ["Income", "Expense"]
+    @State public var newType = "Expense"
+    
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    TextField("Description", text: $newDescription)
+                    TextField("Amount", value:$newAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section {
+                    Picker("Type", selection: $newType) {
+                        ForEach(types, id:\.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section  {
+                    Text("New Transaction")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .bold()
+                    //section title???
+                    let newTransaction = Transaction(description: newDescription, amount: newAmount, type: newType)
+                    Text("Description: \(newTransaction.description)")
+                    Text("Amount: \(newTransaction.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                    Text("Type: \(newTransaction.type)")
+                }
+                .navigationTitle("Enter Transaction")
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
